cb810f20c59c8ec835c833f789379c71c0bca092
tlesner <tjlesner@gmail.com>
2025-04-20 23:08:57 -0600

    cypress libraries, testing and general structure.

---
+++ Develop/cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+	component: {
+		port: 5173,
+		devServer: {
+			framework: 'react',
+			bundler: 'vite',
+			viteConfig,
+		},
+	},
+
+	e2e: {
+		baseUrl: 'http://localhost:3001',
+		setupNodeEvents(on, config) {},
+	},
+});
