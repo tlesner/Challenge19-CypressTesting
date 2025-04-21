b163a0be964f840ea1530970acc25b81c1e90454
tlesner <tjlesner@gmail.com>
2025-04-21 07:16:14 -0600

    added some testing to quiz file

--- Develop/cypress/component/quiz.cy.jsx
+++ Develop/cypress/component/quiz.cy.jsx
@@ -1,16 +1,27 @@
-import Quiz from "../../client/src/components/Quiz";
+import Quiz from '../../client/src/components/Quiz';
 import React from "react";
+import { testQuestions } from '../fixtures/questions.json'
+import '@testing-library/cypress/add-commands';
+
 
 describe('Quiz Component', () => {
     beforeEach(() => {
       cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/questions/random'
-        },
-        {
-          fixture: 'questions.json',
-          statusCode: 200
-        }
-        ).as('randomizeQuestions')
+        { method: 'GET', url: '/api/questions/random' },
+        { fixture: 'questions.json', statusCode: 200 }
+        ).as('getQuestions')
       });
+
+    it('Should render the Quiz on the page.', () => {
+		cy.mount(<Quiz />);
+	});
+
+    it('Should have a button that reads "Start Quiz".', () => {
+        cy.mount(<Quiz />);
+        cy.get('button').contains('Start Quiz').click();
+    });
+    it('', () => {
+        cy.mount(<Quiz />);
+
+    });
+});      
