3cc693eb7f8cdbdcd351cfb4d6102720ec97b829
tlesner <tjlesner@gmail.com>
2025-04-21 07:37:25 -0600

    completed quiz component(?)

--- Develop/cypress/component/quiz.cy.jsx
+++ Develop/cypress/component/quiz.cy.jsx
@@ -1,6 +1,5 @@
 import Quiz from '../../client/src/components/Quiz';
 import React from "react";
-import { testQuestions } from '../fixtures/questions.json'
 import '@testing-library/cypress/add-commands';
 
 
@@ -20,8 +19,25 @@ describe('Quiz Component', () => {
         cy.mount(<Quiz />);
         cy.get('button').contains('Start Quiz').click();
     });
-    it('', () => {
-        cy.mount(<Quiz />);
 
+    it('Should let the user answer questions and complete the quiz', () => {
+        cy.mount(<Quiz />);
+        // Checks if the start button and first question is displayed
+        cy.get('button').contains('Start Quiz').click();
+        cy.get('h2').should('exist');
+        //Checks for question answered
+        cy.get('button').contains('1').click();
+        //Once test is complete, is should comfirm completion
+        cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
     });
-});      
+    it('Should let the user restart the Quiz', () => {
+        cy.mount(<Quiz />);
+        cy.get('button').contains('Start Quiz').click();
+        //Checks for question answered
+        cy.get('button').contains('1').click();
+        //Should let user restart quiz.
+        cy.get('button').contains('Take New Quiz').click();
+        //confirm the test is started
+        cy.get('button').should('exist');
+    });
+});
